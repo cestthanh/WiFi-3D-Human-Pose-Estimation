46a29ce9c947b7f2f8e5f9d3dc81233425d64de0
NguyenTuanThanh <thanh1qaz7ujm@gmail.com>
2026-04-23 00:32:07 +0700

    remove WiPose dataset

--- model/__init__.py
+++ model/__init__.py
@@ -6,6 +6,4 @@ from .HPE_no_denoiser import OriginalHPE
 from .HPE_one_denoiser import OneLayerDenoiserHPE, OneStageAE
 from .HPE_three_denoiser import ThreeLayerDenoiserHPE, ThreeStageAE
 from .HPE_two_denoiser import TwoLayerDenoiserHPE, TwoStageAE
-from .HPE_Wipose import HPEWiPoseModel
 from .sknet_trans_mmfi import DSKNetTransMMFI
-from .sknet_trans_wipose import DSKNetTransWipose
